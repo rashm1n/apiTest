9966366a428b6a80bef5154c1551fb3b5d0875ef
ravindurashmin <rashmin.ravindu@gmail.com>
2019-08-13 12:29:43 +0530

    Initial commit

---
+++ src/main/java/testApp/controller/Controller.java
@@ -0,0 +1,15 @@
+package testApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
